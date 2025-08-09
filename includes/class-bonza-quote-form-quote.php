c9aed60a146445ce16ec3873de93f9efe97ab9a1
Jasper Poly <jasper@inspire.scot>
2025-08-10 03:30:44 +0800

    Init data model

---
+++ includes/class-bonza-quote-form-quote.php
@@ -0,0 +1,125 @@
+<?php
+
+/**
+ * The Quote Model Class
+ *
+ * @link       https://github.com/iamceejay
+ * @since      1.0.0
+ *
+ * @package    Bonza_Quote_Form
+ * @subpackage Bonza_Quote_Form/includes
+ */
+
+/**
+ * Model class for handling CRUD operations
+ *
+ * This class defines all the database operations for quotes including
+ * creation, reading, updating, and deletion with validation.
+ *
+ * @since      1.0.0
+ * @package    Bonza_Quote_Form
+ * @subpackage Bonza_Quote_Form/includes
+ * @author     Jasper Cairoane Poly <jaspercairoane.poly@gmail.com>
+ */
+class Bonza_Quote_Form_Quote {
+
+	/**
+	 * Quote ID
+	 *
+	 * @since    1.0.0
+	 * @access   public
+	 * @var      int
+	 */
+	public $id;
+
+	/**
+	 * Quote Name
+	 *
+	 * @since    1.0.0
+	 * @access   public
+	 * @var      string
+	 */
+	public $name;
+
+	/**
+	 * Quote Email
+	 *
+	 * @since    1.0.0
+	 * @access   public
+	 * @var      string
+	 */
+	public $email;
+
+	/**
+	 * Service Type
+	 *
+	 * @since    1.0.0
+	 * @access   public
+	 * @var      string
+	 */
+	public $service_type;
+
+	/**
+	 * Notes
+	 *
+	 * @since    1.0.0
+	 * @access   public
+	 * @var      string
+	 */
+	public $notes;
+
+	/**
+	 * Status
+	 *
+	 * @since    1.0.0
+	 * @access   public
+	 * @var      string
+	 */
+	public $status;
+
+	/**
+	 * Created timestamp
+	 *
+	 * @since    1.0.0
+	 * @access   public
+	 * @var      string
+	 */
+	public $created_at;
+
+	/**
+	 * Updated timestamp
+	 *
+	 * @since    1.0.0
+	 * @access   public
+	 * @var      string
+	 */
+	public $updated_at;
+
+	/**
+	 * Valid statuses
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 * @var      array
+	 */
+	private static $valid_statuses = array( 'pending', 'approved', 'rejected' );
+
+	/**
+	 * Table name
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 * @var      string
+	 */
+	private static $table_name;
+
+	/**
+	 * Constructor
+	 *
+	 * @since    1.0.0
+	 * @param    array    $data    Quote data array
+	 */
+	public function __construct( $data = array() ) {
+		global $wpdb;
+	}
+}
